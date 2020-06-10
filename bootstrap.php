ea2e477211e9c175d6a454d22117ac9ecc0aaeb2
Andre Lazari <andre.lazari@arquivei.com.br>
2020-06-10 15:39:23 -0300

    Remove env loading from bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -2,5 +2,5 @@
 
 require_once("vendor/autoload.php");
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//$dotenv->load();
